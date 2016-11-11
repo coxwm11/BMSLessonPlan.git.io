70b67738c97579970e76c337c82de24432563b27
WENDY M COX <COXW1@scsk12.org>
2016-11-10 23:52:03 -0600

    added files

---
+++ form-to-email.php
@@ -0,0 +1,61 @@
+<?php
+if(!isset($_POST['submit']))
+{
+	//This page should not be accessed directly. Need to submit the form.
+	echo "error; you need to submit the form!";
+}
+$name = $_POST['Wendy Prayer'];
+$visitor_email = $_POST['coxwm11@gmail.com'];
+$message = $_POST['file'];
+
+//Validate first
+if(empty($name)||empty($visitor_email)) 
+{
+    echo "Name and email are mandatory!";
+    exit;
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+
+$email_from = 'coxwm11@gmail.com';//<== update the email address
+$email_subject = "Lesson Plan";
+$email_body = "You have received a lesson plan from $name.\n".
+    "Document attached:\n $message".
+    
+$to = "bmslessonplans@gmail.com";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: thank-you.html');
+
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+   
+?> 
